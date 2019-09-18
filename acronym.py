9ca425963e2049fd93e6257abd66c79f4d0281fc
Karan <50989977+skaranzx16@users.noreply.github.com>
2019-09-18 15:21:36 +0530

    Update acronym.py

--- acronym.py
+++ acronym.py
@@ -1,7 +1,7 @@
 str = input("Enter the string to be converted into an acronym ")
-res = str[0]
+acronym = str[0]
 for i in range (0, len(str)):
     if str[i] == " ":
-        res = res + str[i+1]
-res = res.upper()
-print("The acronym is", res)
+        acronym += str[i+1]
+acronym = acronym.upper()
+print("The acronym is ", acronym)
